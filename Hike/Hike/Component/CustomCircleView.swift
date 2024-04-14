78b2614b1c4d4a2b0e3ea720fa44b8f319b06ee9
phucdh2 <phucdh2@vng.com.vn>
2024-04-14 17:57:32 +0700

    [Hike] Motion Animation

---
+++ Hike/Hike/Component/CustomCircleView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomCircleView.swift
+//  Hike
+//
+//  Created by Do Huu Phuc on 14/4/24.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    
+    @State private var isAnimateGradient: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(colors: [.customIndigoMedium,
+                                            .customSalmonLight],
+                                   startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                                   endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+                                  )
+                ).onAppear {
+                    withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                        isAnimateGradient.toggle()
+                    }
+            }
+            
+            MotionAnimationView()
+        } //: ZSTACK
+        .frame(width: 256, height: 256)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ Hike/Hike/Screen/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Hike
+//
+//  Created by Do Huu Phuc on 14/4/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Setting View")
+    }
+}
+
+#Preview {
+    SettingView()
+}
